83712b2389ed262137df5b46a8ab754a434482ad
Trever Cullen <trevercullen@gmail.com>
2016-11-28 10:48:15 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CAEN Docker Flask App
+
+Flask app used to test and standardize deployments to AWS using Docker.
+All deployment instructions will be documented below.
+
+## Deployment
---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello world!'
+
+# Some simple routing to learn Flask
+
+@app.route('/<name>')
+def name(name = None):
+    return render_template('base.html', name = name)
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+	if request.method == 'POST':
+		return 'Send post request'
+	else:
+		return 'Login Page'
+
+
+@app.route('/user/<username>')
+def user(username):
+	return 'User %s' % username
+
+if __name__ == '__main__':
+    app.run()
